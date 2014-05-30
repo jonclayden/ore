6c19c52f0641c890f76d7527ad0552cff851d7b9
Jon Clayden <code@clayden.org>
2014-05-30 11:30:16 +0100

    Storing last match in caller's environment; adding matched substrings to match object

--- R/match.R
+++ R/match.R
@@ -1,4 +1,4 @@
-ore.search <- function (regex, text)
+ore.search <- function (regex, text, envir=parent.frame())
 {
     if (!is.ore(regex))
         regex <- ore(regex)
@@ -8,22 +8,31 @@ ore.search <- function (regex, text)
     result <- .Call("chariot_search", attr(regex,".compiled"), text, PACKAGE="chariot")
     
     if (is.na(result[[1]]))
-        return (NULL)
+        match <- NULL
     else
     {
-        match <- list(offset=result[[2]][1], length=result[[3]][1])
+        match <- list(offset=result[[2]][1], length=result[[3]][1], text=substr(text,result[[2]][1],result[[2]][1]+result[[3]][1]-1))
         if (attr(regex, "nGroups") > 0)
         {
             match$groups <- list(offsets=result[[2]][-1], lengths=result[[3]][-1])
+            match$groups$text <- sapply(seq_len(length(result[[2]])-1), function(i) substr(text,result[[2]][i+1],result[[2]][i+1]+result[[3]][i+1]-1))
             if (!is.null(attr(regex, "groupNames")))
             {
                 groupNames <- attr(regex, "groupNames")
                 groupNames[is.na(groupNames)] <- ""
                 names(match$groups$offsets) <- groupNames
                 names(match$groups$lengths) <- groupNames
+                names(match$groups$text) <- groupNames
             }
         }
-        
-        return (match)
     }
+    
+    assign(".ore.last", match, envir=envir)
+    return (match)
+}
+
+# NB: allow "replacement" to be a function
+ore.sub <- function (regex, replacement, text, global = FALSE, ...)
+{
+    
 }
