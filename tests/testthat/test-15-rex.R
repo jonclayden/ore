011dd334bea3913e88e7fe8a0e43b8f801508687
Jon Clayden <code@clayden.org>
2014-11-16 09:54:25 +0000

    Skipping rex test if the package is not available

--- tests/testthat/test-15-rex.R
+++ tests/testthat/test-15-rex.R
@@ -1,25 +1,30 @@
 context("Compatibility with rex")
 
 test_that("ore search works with rex output", {
-    suppressPackageStartupMessages(library(rex))
-    regex <- rex(start,
-        ## match the protocol -- may exist or may not
-        maybe(capture(
-            capture(except_some_of(":")),
-            "://"
-        )),
+    if (system.file(package="rex") == "")
+        skip("The \"rex\" package is not available")
+    else
+    {
+        suppressPackageStartupMessages(library(rex))
+        regex <- rex(start,
+            ## match the protocol -- may exist or may not
+            maybe(capture(
+                capture(except_some_of(":")),
+                "://"
+            )),
         
-        ## match the path
-        capture(one_or_more(not(":/"))),
+            ## match the path
+            capture(one_or_more(not(":/"))),
         
-        ## get the port
-        maybe(capture(":", capture(numbers))),
+            ## get the port
+            maybe(capture(":", capture(numbers))),
         
-        ## and the rest
-        maybe(capture("/", anything)),
+            ## and the rest
+            maybe(capture("/", anything)),
         
-        end)
+            end)
     
-    url <- "http://www.clayden.org/"
-    expect_that(ore::matches(ore.search(regex,url)), equals(url))
+        url <- "http://www.clayden.org/"
+        expect_that(ore::matches(ore.search(regex,url)), equals(url))
+    }
 })
