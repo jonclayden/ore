934b850ff3dd71f843e559fb697e07dbc3725654
Jon Clayden <code@clayden.org>
2021-09-07 21:23:53 +0100

    Extending file tests, and adding documentation

--- inst/tinytest/test-12-files.R
+++ inst/tinytest/test-12-files.R
@@ -1,14 +1,38 @@
-# Calling readLines() will convert the text to UTF-8
+# Local iconv support for SHIFT-JIS is necessary for these tests
 if ("SHIFT-JIS" %in% iconvlist())
 {
+    # Four ways to search in a SHIFT-JIS-encoded file:
+    #   1. Use ore.file(), declaring the encoding, and use the internal
+    #      file-handling code to read the file directly.
+    #   2. Use readLines() to read in the file, declaring the encoding,
+    #      which implicitly converts it to UTF-8 because R doesn't understand
+    #      SHIFT-JIS internally. Search in this converted text, bearing in mind
+    #      that byte offsets will reflect UTF-8 encoding, and so won't match
+    #      those in the file.
+    #   3. Use iconv() to recreate a SHIFT-JIS-encoded string in R, and create
+    #      an ore regex to match. The latter's encoding must be specified,
+    #      because R doesn't retain encoding information except for latin1 and
+    #      UTF-8.
+    #   4. Create a connection, declaring the encoding, and pass it directly
+    #      to ore_search() - assuming connection support is available.
     s1 <- ore.search("\\p{Katakana}+", ore.file("sjis.txt",encoding="SHIFT-JIS"))
     con <- file("sjis.txt", encoding="SHIFT-JIS")
-    s2 <- ore.search("\\p{Katakana}+", readLines(con))
+    text <- readLines(con)
+    s2 <- ore.search("\\p{Katakana}+", text)
+    s3 <- ore.search(ore("\\p{Katakana}+",encoding="SHIFT-JIS"), iconv(text,"UTF-8","SHIFT-JIS"))
+    close(con)
+    con <- file("sjis.txt", encoding="SHIFT-JIS")
+    s4 <- ore.search("\\p{Katakana}+", con)
     close(con)
     
-    # Same character offsets but different byte offsets
-    expect_equal(c(s1$offsets,s2$offsets), c(14L,14L))
-    expect_equal(c(s1$byteOffsets,s2$byteOffsets), c(18L,22L))
+    # Check the match was found in each case
+    results <- list(s1, s2, s3, s4)
+    expect_false(any(sapply(results, is.null)))
     
+    # Same character offsets but different byte offsets
+    expect_equal(sapply(results,"[[","offsets"), c(14L,14L,14L,14L))
+    expect_equal(sapply(results,"[[","byteOffsets"), c(18L,22L,18L,18L))
+    
+    # Binary search
     expect_equal(ore::matches(ore.search("\\w+",ore.file("hello.bin",binary=TRUE))), "Hello")
 }
